cb6a68e397ed31d34a14bc2ea59b9eaf73227308
SzyWoj <148621843+SzyWoj@users.noreply.github.com>
2024-04-07 20:32:00 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Sound();
+            cat.Sound();
+
+        }
+    }
+
+    class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("The animal goes *brrr*");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("The dog goes *woof*");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("The cat goes *miau*");
+
+        }
+    }
+}
